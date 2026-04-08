b5558897172450cbb09de92c9bbf7912c092719f
JoseHerrera <jlhm789@gmail.com>
2026-04-08 16:26:50 -0500

    admin

---
+++ tienda/management/commands/createsu.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+import os
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+            self.stdout.write("Superusuario creado")
+        else:
+            self.stdout.write("El superusuario ya existe")
